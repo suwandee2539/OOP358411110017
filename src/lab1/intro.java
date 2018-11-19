15dc1f491d3c8ca4f757fedf641f0b6dac592a20
suwandee <fern0816937525@gmail>
2018-11-19 16:34:30 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,23 @@
 package lab1;
 
 public class intro {
-}
+    public static void main(String[] args) {
+
+        System.out.println("suwandee rattana-anujorn");
+        System.out.println("fern0816937525@gmail.com");
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+}//class
