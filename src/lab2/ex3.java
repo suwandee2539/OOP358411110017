14287f12ec4423152f392d498432bf69a0fe03d1
suwandee <fern0816937525@gmail>
2018-11-26 15:52:46 +0700

    Lab 2: IF-ELSE-IF

---
+++ src/lab2/ex3.java
@@ -0,0 +1,25 @@
+package lab2;
+
+public class ex3 {
+    public static void main(String[] args) {
+        //if-else-if
+        //simple Grade calculator
+        //F=0-49, D=50-59, C=60-69, B=70-79, A=80-100
+        int score = 50;
+        if (score >=0 && score <=49)
+            System.out.println("Grade F.");
+        else if (score >=0 && score <=59)
+            System.out.println("Grade D");
+        else if (score >=0 && score <=69)
+            System.out.println("Grade/ C");
+        else if (score >=0 && score <=79)
+            System.out.println("Grade B");
+        else if (score >=0 && score <=100)
+            System.out.println("Grade A");
+        else
+            System.out.println("Invalid Score.             ");
+
+
+
+    }//main
+}
---
+++ src/lab2/ez2.java
@@ -0,0 +1,25 @@
+package lab2;
+
+public class ez2 {
+
+    public static void main(String[] args) {
+        //if
+        int x = 10;
+        if (x<10){
+            System.out.println("x less than 10.");
+        }else{
+            System.out.println("x equal or more than 10.");
+        }
+
+
+
+
+        System.out.println(x);
+
+
+
+
+
+
+    }//main
+}
