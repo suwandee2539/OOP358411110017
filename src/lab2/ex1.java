7f900c0c9d0653a5d63c9d7e9f17b0cb611064cc
suwandee <fern0816937525@gmail>
2018-11-26 14:34:15 +0700

    Lab 2: Variables

---
+++ src/lab2/ex1.java
@@ -0,0 +1,46 @@
+package lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        //integer
+        int a;
+        a=10;
+        System.out.println(a);
+        int b = 20;
+        System.out.println(b);
+        int c = a + b;
+        System.out.println("ผลรวมของ a และ b เท่ากับ "+c);
+
+        //real number
+        double d=5.5;
+        System.out.println(d);
+        d = d * 10;
+        System.out.println(d);
+
+
+        //character
+        char e = '#';
+        System.out.println(e);
+
+        //String
+        String str = "Suwandee Rattana-andjon";
+        System.out.println("My Name Is "+str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+}//class
