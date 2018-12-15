9cfa03dceaf7684948f466cf0ee531cbfd592a93
suwandee <fern0816937525@gmail>
2018-12-15 17:18:58 +0700

    Lab 2: IF-ELSE-IF

---
+++ src/lab3/ex1.java
@@ -0,0 +1,26 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        String name;
+        Scanner sc = new Scanner(System.in);
+        //name
+        System.out.print("What is your nickname? : ");
+        name = sc.nextLine();
+        System.out.println("Hello, "+ name);
+
+        //age
+        System.out.print("How old are you? : ");
+        int age = sc.nextInt();
+        System.out.println("You are "+age+" years old. ");
+
+        if ( age >30) System.out.println("You are older. ");
+        else System.out.println("You are younger. ");
+
+
+
+    }//main
+}//class
---
+++ src/lab3/ex2.java
@@ -0,0 +1,40 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        String name;
+        Scanner sc = new Scanner(System.in);
+        //name
+        System.out.print("ชื่อ นามสกุล ? : ");
+        name = sc.nextLine();
+        System.out.println("สวัสดี, "+ name);
+
+        //age
+        System.out.print("อายุ ? : ");
+        int age = sc.nextInt();
+        System.out.println("คุณ อายุ "+age+" ปี ");
+
+
+        //address
+        System.out.print("ที่อยู่ ? : ");
+        int address = sc.nextInt();
+        System.out.println("ที่อยู๋, "+ address);
+
+        //telephone
+        System.out.print("เบอร์โทรศัพท์ ? : ");
+        int telephone = sc.nextInt();
+        System.out.println("เบอร์โทรศัพท์, "+ telephone );
+
+        String sex;
+        Scanner se = new Scanner(System.in);
+        //sex
+        System.out.print("เพศ ? : ");
+        sex = se.nextLine();
+        System.out.println("เพศ, "+ sex);
+
+    }//main
+}//class
+
