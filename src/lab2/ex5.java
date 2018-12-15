9846843c8650143f32777aee3c5022781a285ef1
suwandee <fern0816937525@gmail>
2018-12-15 15:06:23 +0700

    Lab 2: IF-ELSE-IF

---
+++ src/lab2/ex5.java
@@ -0,0 +1,16 @@
+package lab2;
+
+public class ex5 {
+    public static void main(String[] args) {
+
+        for (int x = 0; x <= 100; x++){
+            if (x %3==0)
+            if (x %5==0)
+                System.out.println(x);
+
+        }
+    }//main
+}//class
+
+
+
---
+++ src/lab2/ex7.java
@@ -0,0 +1,14 @@
+package lab2;
+
+public class ex7 {
+    public static void main(String[] args) {
+
+        int x =1;
+        do{
+            System.out.println("Hello." +x);
+        }while (++x <= 10);
+
+
+
+    }//main
+}//calss
