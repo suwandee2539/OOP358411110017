cbca6316bdf91082ae5f08b66531f4c56c71a068
suwandee <fern0816937525@gmail>
2018-12-24 15:29:36 +0700

    Lab 6: String

--- src/lab6/ex3.java
+++ src/lab6/ex3.java
@@ -1,7 +1,27 @@
 package lab6;
 
+import java.util.StringTokenizer;
+
 public class ex3 {
     public static void main(String[] args) {
+        String myMsg = "I am work at RUTS 2018";
+        StringBuffer strBur = new StringBuffer(myMsg);
 
-    }
-}
+        System.out.println(strBur);
+        System.out.println(strBur.reverse());
+        strBur.append(" Happy New Year, 2019");
+        System.out.println(strBur);
+
+        //StringTokenizer
+        StringTokenizer token = new StringTokenizer(strBur.toString());
+
+        System.out.println(token);
+        System.out.println(token.countTokens());
+
+        //cutting words from String
+        while (token.hasMoreElements()){
+            System.out.println(token.nextToken());
+        }
+
+    }//main
+}//class
