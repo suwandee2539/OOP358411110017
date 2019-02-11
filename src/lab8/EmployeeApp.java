5504ef88276f8f1a9c6ec7b1603e518a214164d6
suwandee <fern0816937525@gmail>
2019-02-11 14:11:04 +0700

    Lab 8:OOP-Inherit

---
+++ src/lab8/EmployeeApp.java
@@ -0,0 +1,14 @@
+package lab8;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+
+        //create objects
+        Employee emp = new Employee("358411110017","SuwandeeRattana-anujon","Programmer",25000.00);
+        System.out.println(emp.toString());
+
+
+
+
+    }//main
+}//class
